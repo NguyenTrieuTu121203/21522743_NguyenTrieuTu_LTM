2660702ecb68854eed9baef0a200b4108ae60f23
NguyenTrieuTu121203 <21522743@gn.uit.edu.vn>
2023-03-30 15:34:23 +0700

    first commit

---
+++ Lythuyet_ThucHanh_LapTrinhMang/Week3/BaiTapTuan3/Sever_Client_Chat/Client/ClientForm.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Client
+{
+    public partial class ClientForm : Form
+    {
+
+        string IpLocal="127.0.0.1";
+        int Port = 9999;
+        Socket Client;
+        public ClientForm()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void ClientForm_Load(object sender, EventArgs e)
+        {
+            lsViewMessage.View = View.List;
+            txtIP_Address.Text = IpLocal;
+            txtPort.Text = Port.ToString();
+            txtIP_Address.Text = IpLocal;
+            txtIP_Address.Enabled = false;
+            txtPort.Enabled = false;    
+        }
+
+        private void btnConnectSever_Click(object sender, EventArgs e)
+        {
+            
+            // xử lý đụng độ khi sử dụng tài nguyên 
+            CheckForIllegalCrossThreadCalls = false;
+            if (ConnectToSever())
+            {
+                MessageBox.Show("Kết nối thành công với Sever", "Thông báo", MessageBoxButtons.OK);
+                // khi kết nối thành công sẽ không cho nhấn nút kết nối nữa
+                btnConnectSever.Enabled = false;
+            }
+            
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtMessage.Text))
+            {
+                MessageBox.Show("Chưa nhập tin nhắn", "Error", MessageBoxButtons.OK);
+                txtMessage.Focus();
+            }
+            else
+            {
+                ClientSend();
+                lsViewMessage.Items.Add(">> Client: " + txtMessage.Text);
+                txtMessage.Clear();
+            }
+            
+        }
+
+
+        private bool ConnectToSever()
+        {
+            IPEndPoint Ip = new IPEndPoint(IPAddress.Parse(IpLocal), Port);
+            Client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            
+            try
+            {
+                // Cố gắng kết nối 
+                Client.Connect(Ip);
+            }
+            catch
+            {
+                MessageBox.Show("Không kết nối được", "Error", MessageBoxButtons.OK);
+                return false;
+            }
+            // Tạo luồng để nhận dữ liệu từ sever
+            Thread Listen = new Thread(ClientReciveData);
+            Listen.IsBackground = true;
+            Listen.Start();
+            return true;
+
+        }
+
+        private void ClientSend()
+        {
+           
+            if (Client != null)
+            {
+                byte[] ByteMessage;
+                ByteMessage = ConvertStringToByte(">> Client: " + txtMessage.Text);
+                Client.Send(ByteMessage);
+            }
+            else
+            {
+                MessageBox.Show("Bạn nhấn nút Connect để Sever có thể nhận dược tin nhắn! ", "Error", MessageBoxButtons.OK);
+            }
+
+        }
+        private void ClientReciveData()
+        {
+            try
+            {
+                while (true)
+                {
+                    //khai báo một mảng byte để chứa dữ liệu
+                    byte[] Data = new byte[1024 * 5000];
+                    // đổ dữ liệu vừa nhận được vào mảng byte  
+                    Client.Receive(Data);
+                    // chuyển dữ liệu vừa nhận thành chuỗi 
+                    string StringMessage = ConvertByteToString(Data);
+                    lsViewMessage.Items.Add(StringMessage);
+                }
+            }
+            catch
+            {
+                Close();
+            }
+
+        }
+        private void ClientCloseConnet()
+        {
+            Client.Close();
+        }
+        
+        private string ConvertByteToString(byte[] data)
+        {
+            return Encoding.UTF8.GetString(data);
+        }
+
+        private byte[] ConvertStringToByte(string data)
+        {
+            return Encoding.UTF8.GetBytes(data);
+        }
+
+        
+    }
+}
---
+++ Lythuyet_ThucHanh_LapTrinhMang/Week3/BaiTapTuan3/Sever_Client_Chat/Sever/SeverForm.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Sever
+{
+    public partial class SeverForm : Form
+    {
+
+        int Port = 9999;
+        Socket sever;
+        List<Socket> ClientList;
+
+        public SeverForm()
+        {
+            InitializeComponent();
+            // Xử lý đụng độ
+            CheckForIllegalCrossThreadCalls =false;
+            // Kết nối khi Form được mở 
+            Connected_Clients();
+        }
+
+        private void SeverForm_Load(object sender, EventArgs e)
+        {
+            lsViewMessageSever.View = View.List;
+        }
+        private void btnSeverSend_Click(object sender, EventArgs e)
+        {
+            // Kiểm tra dữ liệu nhập vào
+            if (string.IsNullOrEmpty(txtMessageSever.Text))
+            {
+                MessageBox.Show("Chưa nhập tin nhắn", "Error", MessageBoxButtons.OK);
+                txtMessageSever.Focus();
+            }
+            else
+            {
+                // Duyệt qua tất cả client và gửi dữ liệu qua các client có trong danh sách
+                foreach (Socket client in ClientList) SeverSend(client);
+                // Đưa vào list view
+                lsViewMessageSever.Items.Add(">> Sever: " + txtMessageSever.Text);
+                txtMessageSever.Clear();
+            }
+            
+        }
+
+        private void Connected_Clients()
+        {
+            
+            ClientList = new List<Socket>();
+            IPEndPoint Ip = new IPEndPoint(IPAddress.Any, Port);
+            sever = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            sever.Bind(Ip);
+
+            
+            Thread Listen = new Thread(() =>
+            {
+                try
+                {
+                    while (true)
+                    {
+                        sever.Listen(100);
+                        Socket client = sever.Accept();
+                        ClientList.Add(client);
+                        // Tạo luồng đề nhận dữ liệu từ các client
+                        Thread Receive = new Thread(SeverReciveData);
+                        Receive.IsBackground = true;
+                        Receive.Start(client);
+                    }
+                }
+                catch
+                {
+                    // Hai câu lệnh bên dưới dùng để tránh lỗi khi có một hoặc nhiều client bất ngờ ngắt kết nối
+
+                    Ip = new IPEndPoint(IPAddress.Any, Port);
+                    sever= new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                }
+            });
+            Listen.IsBackground = true;
+            Listen.Start();
+
+        }
+
+        // Sever gửi dữ liệu đến các client
+        private void SeverSend(Socket client)
+        {
+            byte[] ByteMessage;
+            ByteMessage = ConvertStringToByte(">> Sever: "+txtMessageSever.Text);
+            client.Send(ByteMessage);
+        }
+        private void SeverReciveData(object obj)
+        {
+            Socket client = obj as Socket;
+            try
+            {
+                while (true)
+                {
+                    // Tạo một mảng byte để chứa dữ liệu
+                    byte[] Data = new byte[1024 * 5000];
+                    client.Receive(Data);
+                    string StringMessage = ConvertByteToString(Data);
+                    lsViewMessageSever.Items.Add(StringMessage);
+                }
+            }
+            catch
+            {
+                // Xóa client ngắt kết nối ra khỏi danh sách client
+                ClientList.Remove(client);
+                client.Close();
+            }
+
+        }
+        private void ClientCloseConnet()
+        {
+            sever.Close();
+        }
+
+        private string ConvertByteToString(byte[] data)
+        {
+            return Encoding.UTF8.GetString(data);
+        }
+
+        private byte[] ConvertStringToByte(string data)
+        {
+            return Encoding.UTF8.GetBytes(data);
+        }
+
+        
+    }
+}
