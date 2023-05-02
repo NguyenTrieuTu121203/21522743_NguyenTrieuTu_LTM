c619916c3b210c7b22c52e2b85fba486e6d91d2b
NguyenTrieuTu121203 <128152845+NguyenTrieuTu121203@users.noreply.github.com>
2023-05-02 19:19:35 +0700

    Add files via upload

---
+++ Lythuyet_ThucHanh_LapTrinhMang/Week6/Lab03_Nhom8_21522743/Bai3/TCP Client Server/App/App.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace App
+{
+    partial class App
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sv_Btn = new System.Windows.Forms.Button();
+            this.cl_Btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // sv_Btn
+            // 
+            this.sv_Btn.Location = new System.Drawing.Point(61, 170);
+            this.sv_Btn.Name = "sv_Btn";
+            this.sv_Btn.Size = new System.Drawing.Size(300, 93);
+            this.sv_Btn.TabIndex = 0;
+            this.sv_Btn.Text = "Server";
+            this.sv_Btn.UseVisualStyleBackColor = true;
+            this.sv_Btn.Click += new System.EventHandler(this.sv_Btn_Click);
+            // 
+            // cl_Btn
+            // 
+            this.cl_Btn.Location = new System.Drawing.Point(435, 170);
+            this.cl_Btn.Name = "cl_Btn";
+            this.cl_Btn.Size = new System.Drawing.Size(300, 93);
+            this.cl_Btn.TabIndex = 1;
+            this.cl_Btn.Text = "Add Client";
+            this.cl_Btn.UseVisualStyleBackColor = true;
+            this.cl_Btn.Click += new System.EventHandler(this.cl_Btn_Click);
+            // 
+            // App
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::App.Properties.Resources.full;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cl_Btn);
+            this.Controls.Add(this.sv_Btn);
+            this.Name = "App";
+            this.Text = "App";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button sv_Btn;
+        private System.Windows.Forms.Button cl_Btn;
+    }
+}
+
---
+++ Lythuyet_ThucHanh_LapTrinhMang/Week6/Lab03_Nhom8_21522743/Bai3/TCP Client Server/App/App.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Server;
+using TCP_Client_Server;
+using System.Threading;
+namespace App
+{
+
+    public partial class App : Form
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        Thread _thr;
+        private void sv_Btn_Click(object sender, EventArgs e)
+        {  
+            _thr = new Thread(SpawnServer);
+            _thr.IsBackground = true;
+            _thr.Start();
+             
+        }
+
+        private void cl_Btn_Click(object sender, EventArgs e)
+        {
+            Thread thread = new Thread(SpawnClient);
+            thread.IsBackground = true;
+            thread.Start();
+        }
+
+        void Sv_toggle()
+        {
+            if (_thr.IsAlive)
+            {
+                sv_Btn.Enabled = false;
+            }
+            else
+            {
+                sv_Btn.Enabled=true;
+            }
+        }
+
+        public void SpawnClient()
+        {
+            TCP_Client_Server.Client ClientUI = new TCP_Client_Server.Client();
+            ClientUI.ShowDialog();
+        }
+
+        public void SpawnServer()
+        {
+            sv_Btn.Enabled = false;
+            Server.Server ServerUI = new Server.Server();
+            ServerUI.ShowDialog();
+            if (ServerUI._Closed)
+            {
+                sv_Btn.Enabled = true;
+            }
+        }
+    }
+}
