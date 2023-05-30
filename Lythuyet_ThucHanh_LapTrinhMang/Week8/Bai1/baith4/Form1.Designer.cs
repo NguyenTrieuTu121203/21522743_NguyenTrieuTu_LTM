e3694fc6278607eae25fd97c04bf3fce84b79d97
NguyenTrieuTu121203 <128152845+NguyenTrieuTu121203@users.noreply.github.com>
2023-05-30 08:54:32 +0700

    Add files via upload

---
+++ Lythuyet_ThucHanh_LapTrinhMang/Week8/Bai1/baith4/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace baith4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lsData = new System.Windows.Forms.ListView();
+            this.btnGet = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(39, 98);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(552, 45);
+            this.txtInput.TabIndex = 0;
+            // 
+            // lsData
+            // 
+            this.lsData.HideSelection = false;
+            this.lsData.Location = new System.Drawing.Point(39, 168);
+            this.lsData.Name = "lsData";
+            this.lsData.Size = new System.Drawing.Size(552, 223);
+            this.lsData.TabIndex = 1;
+            this.lsData.UseCompatibleStateImageBehavior = false;
+            this.lsData.View = System.Windows.Forms.View.SmallIcon;
+            // 
+            // btnGet
+            // 
+            this.btnGet.Location = new System.Drawing.Point(597, 98);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(191, 45);
+            this.btnGet.TabIndex = 2;
+            this.btnGet.Text = "Get";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGet);
+            this.Controls.Add(this.lsData);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.ListView lsData;
+        private System.Windows.Forms.Button btnGet;
+    }
+}
---
+++ Lythuyet_ThucHanh_LapTrinhMang/Week8/Bai1/baith4/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace baith4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnGet_Click(object sender, EventArgs e)
+        {
+            string url = txtInput.Text;
+            string html = await GetHtmlFromUrl(url);
+
+            // Xóa dữ liệu hiện có trong listView1
+            lsData.Items.Clear();
+
+            // Hiển thị nội dung HTML trong listView1
+            lsData.Items.Add(html);
+        }
+
+        private async Task<string> GetHtmlFromUrl(string url)
+        {
+            string html = string.Empty;
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    response.EnsureSuccessStatusCode();
+                    html = await response.Content.ReadAsStringAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                // Xử lý ngoại lệ (nếu cần thiết)
+                MessageBox.Show("Đã xảy ra lỗi khi tải trang web: " + ex.Message);
+            }
+
+            return html;
+        }
+
+        
+    }
+}
