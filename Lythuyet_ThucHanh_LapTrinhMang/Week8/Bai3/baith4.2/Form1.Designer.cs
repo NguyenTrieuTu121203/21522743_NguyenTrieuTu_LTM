ccfb18bbd5ea76348da23e43029523f27259409d
NguyenTrieuTu121203 <128152845+NguyenTrieuTu121203@users.noreply.github.com>
2023-05-30 09:11:39 +0700

    Add files via upload

---
+++ Lythuyet_ThucHanh_LapTrinhMang/Week8/Bai3/baith4.2/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace baith4._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.txtInputData = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(800, 450);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // txtInputData
+            // 
+            this.txtInputData.Location = new System.Drawing.Point(12, 12);
+            this.txtInputData.Name = "txtInputData";
+            this.txtInputData.Size = new System.Drawing.Size(502, 22);
+            this.txtInputData.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(537, 12);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(82, 26);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(653, 12);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(82, 26);
+            this.btnDownload.TabIndex = 3;
+            this.btnDownload.Text = "Down";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtInputData);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox txtInputData;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnDownload;
+    }
+}
+
---
+++ Lythuyet_ThucHanh_LapTrinhMang/Week8/Bai3/baith4.2/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+namespace baith4._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private async void btnSearch_Click(object sender, EventArgs e)
+        {
+            string url = txtInputData.Text;
+            string outputFolder = @"C:\Users\Ngo Minh Quan\source\repos\baith4.2\baith4.2\src";
+            string filePath = Path.Combine(outputFolder, "webpage.html");
+
+            await DownloadAndSaveHtml(url, filePath);
+
+            // Hiển thị nội dung trang web lên form
+            string html = File.ReadAllText(filePath);
+            webBrowser1.DocumentText = html;
+        }
+
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            string url = txtInputData.Text;
+            string outputFolder = @"C:\Users\Ngo Minh Quan\source\repos\baith4.2\baith4.2\src";
+
+            DownloadResources(url, outputFolder);
+        }
+
+        private async Task DownloadAndSaveHtml(string url, string filePath)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    response.EnsureSuccessStatusCode();
+
+                    using (Stream contentStream = await response.Content.ReadAsStreamAsync(),
+                                  fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await contentStream.CopyToAsync(fileStream);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Xử lý ngoại lệ (nếu cần thiết)
+                MessageBox.Show("Đã xảy ra lỗi khi tải và lưu trang web: " + ex.Message);
+            }
+        }
+
+        
+
+        
+        private async void DownloadResources(string url, string outputFolder)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    string html = await client.GetStringAsync(url);
+
+                    // Tạo một thư mục mới nếu thư mục đầu ra chưa tồn tại
+                    Directory.CreateDirectory(outputFolder);
+
+                    HtmlAgilityPack.HtmlDocument htmlDocument = new HtmlAgilityPack.HtmlDocument();
+                    htmlDocument.LoadHtml(html);
+
+                    var resourceUrls = GetResourceUrls(htmlDocument);
+
+                    foreach (var resourceUrl in resourceUrls)
+                    {
+                        string fileName = Path.GetFileName(resourceUrl);
+                        string outputPath = Path.Combine(outputFolder, fileName);
+
+                        using (HttpResponseMessage response = await client.GetAsync(resourceUrl))
+                        using (Stream contentStream = await response.Content.ReadAsStreamAsync())
+                        using (FileStream fileStream = new FileStream(outputPath, FileMode.Create))
+                        {
+                            await contentStream.CopyToAsync(fileStream);
+                        }
+                    }
+
+                    MessageBox.Show("Tải xuống thành công!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi tải xuống: " + ex.Message);
+                }
+            }
+        }
+
+        private List<string> GetResourceUrls(HtmlAgilityPack.HtmlDocument htmlDocument)
+        {
+            List<string> resourceUrls = new List<string>();
+
+            var imgElements = htmlDocument.DocumentNode.SelectNodes("//img[@src]");
+            var linkElements = htmlDocument.DocumentNode.SelectNodes("//link[@href]");
+            var scriptElements = htmlDocument.DocumentNode.SelectNodes("//script[@src]");
+
+            if (imgElements != null)
+            {
+                foreach (var imgElement in imgElements)
+                {
+                    string imgUrl = imgElement.GetAttributeValue("src", "");
+                    resourceUrls.Add(imgUrl);
+                }
+            }
+
+            if (linkElements != null)
+            {
+                foreach (var linkElement in linkElements)
+                {
+                    string linkUrl = linkElement.GetAttributeValue("href", "");
+                    resourceUrls.Add(linkUrl);
+                }
+            }
+
+            if (scriptElements != null)
+            {
+                foreach (var scriptElement in scriptElements)
+                {
+                    string scriptUrl = scriptElement.GetAttributeValue("src", "");
+                    resourceUrls.Add(scriptUrl);
+                }
+            }
+
+            return resourceUrls;
+        }
+
+        
+    }
+}
